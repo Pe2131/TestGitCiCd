cb294fdd5680dbba277d7bba093c8feee9bea37c
peyman <peyman.dgn@outlook.com>
2022-06-06 11:55:23 +0430

    add somw another weather to list

--- TestGitCiCd/Controllers/WeatherForecastController.cs
+++ TestGitCiCd/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestGitCiCd.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","humid"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
