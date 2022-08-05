e34b5867fdaa42e895e4c7eb8b353cfc7008a3c8
peyman <peyman.dgn@outlook.com>
2022-08-05 11:17:18 +0430

    add new controller

---
+++ TestGitCiCd/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestGitCiCd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("peyman");
+        }
+    }
+}
